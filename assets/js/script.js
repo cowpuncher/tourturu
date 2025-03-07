5b4952e1035aa259e6f4ebb9ef5316ea946f69a5
WebDev TVlean <tvleanjohnny@gmail.com>
2025-03-07 15:17:16 +0300

    adaptive

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,24 @@ document.addEventListener('DOMContentLoaded', function() {
         scrollbar: {
           el: '.swiper-scrollbar',
         },
+        // Responsive breakpoints
+        breakpoints: {
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+          },
+          // when window width is >= 480px
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20
+          },
+          // when window width is >= 640px
+          993: {
+            slidesPerView: 4,
+            spaceBetween: 20
+          }
+        }
     });
 
     const swiperOne = new Swiper('.swiper-one', {
